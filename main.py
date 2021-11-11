d3c20bdede70a8477549837ce160a989ea827940
Ritesh Srivastava <riteshsrivastava123456@gmail.com>
2021-11-11 21:48:32 +0530

    uploading the code

---
+++ main.py
@@ -0,0 +1,75 @@
+import curses
+from curses import wrapper
+import time
+import random
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(1, 0, "This is the Speed typing test...")
+    stdscr.addstr("\nPress any key to begin...")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(5,0, f"WPM: {wpm}")
+
+    for i,char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if correct_char != char:
+            color = curses.color_pair(2)
+            char = correct_char
+        stdscr.addstr(0,i, char, color)
+
+def load_text():
+    with open("text.txt","r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+def wpm_test(stdscr):
+    target_text = load_text()
+    current_text = []
+    wpm=0
+    start_time =  time.time()
+    stdscr.nodelay(True)
+
+    while True:
+        time_elapsed = max(time.time()- start_time,1)
+        wpm = round((len(current_text)/ (time_elapsed/60))/5)
+        stdscr.clear()
+        display_text(stdscr,target_text, current_text, wpm)
+        stdscr.refresh()
+
+        if target_text == "".join(current_text):
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text)>0:
+                current_text.pop()
+        elif len(current_text)<len(target_text):
+            current_text.append(key)
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(6,0, "You completed the text! Press any key to try again or esc to close...")
+        key = stdscr.getkey()
+
+        if ord(key)==27:
+            break
+
+wrapper(main)
